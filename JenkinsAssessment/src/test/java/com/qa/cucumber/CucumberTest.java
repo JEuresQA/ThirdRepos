36c4ca854c7df408e961c591fc4cee79333fa681
JEuresQA <Jonathan.Eures@academytrainee.com>
2019-03-01 15:30:51 +0000

    now with partial code for task 2

---
+++ JenkinsAssessment/src/test/java/com/qa/cucumber/CucumberTest.java
@@ -0,0 +1,12 @@
+package com.qa.cucumber;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "C:\\Users\\Owner\\eclipse-workspace\\JenkinsAssessment\\FeatureFileUpdateUser.feature")
+public class CucumberTest {
+
+}
